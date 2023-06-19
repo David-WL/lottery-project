21dfb6926682cef7e4ea2d12e3437bfd514cb675
David-WL <lwbpupt@hotmail.com>
2023-06-20 07:51:41 +0800

    add test case for lottery smart contracts

--- contracts/Lottery.sol
+++ contracts/Lottery.sol
@@ -4,150 +4,179 @@ import "@openzeppelin/contracts/utils/Strings.sol";
 
 pragma solidity ^0.8.9;
 
-contract Lottery /*is Ownable*/{
-    address payable owner; 
-    uint minWager = 1; 
-    uint totalWager = 0; 
-    uint numberOfWagers = 0;   
-    uint constant MAX_NUMBER_OF_WAGERS = 2; 
-    uint winningNumber = 999; 
-    uint constant MAX_WINNING_NUMBER = 3; 
-    address payable [] playerAddresses;
-    mapping (address => bool) playerAddressesMapping;
-    struct Player { 
-        uint amountWagered; 
-        uint numberWagered; 
+/*is Ownable*/ contract Lottery {
+    // record the owner of the contract
+    // will be replaced later by openzeppelin Ownable
+    address payable owner;
+
+    // minimum amount to wage (wei)
+    uint constant MIN_WAGER = 1000;
+
+    // maximum number of players allowed for each round
+    uint constant MAX_NUMBER_OF_PLAYERS = 3;
+
+    // range of numbers to bet, from 1 to MAX_NUMBER_TO_BET
+    uint constant MAX_NUMBER_TO_BET = 10;
+
+    // total amount waged by players so far
+    uint totalWager = 0;
+
+    // number of players waged so far
+    uint numberOfPlayers = 0;
+
+    uint constant MAX_NUMBER_OF_WAGERS = 2;
+
+    // winning number
+    uint winningBetNumber = 0;
+
+    // array of addresses of all players
+    address payable[MAX_NUMBER_OF_PLAYERS] playerAddressesArray;
+
+    // waging details of all players in a mapping object
+    mapping(address => bool) playerAddressesMapping;
+
+    // store wager details of each player
+    struct Player {
+        uint amountWagered;
+        uint numberWagered;
     }
-    mapping(address => Player) playerDetails;
+
+    mapping(address => Player) playerDetailsMapping;
 
     // the event to announce the winning number
-    event WinningNumber(
-        uint number
-    );
-    
+    event Winner(address winner, uint amount);
+
     // the event to display the status of the game
-    event Status (
-        uint players,
-        uint maxPlayers
-    );
+    event Status(uint players, uint maxPlayers);
 
     // the constructor for the contract
-    constructor(uint _minWager) {        
+    constructor() {
         owner = payable(msg.sender);
-        if (_minWager >0) minWager = _minWager;
+        //if (_minWager >0) minWager = _minWager;
     }
 
     function bet(uint number) public payable {
-        // you check using the mapping for performance reasons
-        require(playerAddressesMapping[msg.sender] == false);
-        
-        // check the range of numbers allowed
-        require(number >=1 && number <= MAX_WINNING_NUMBER);
-        
-        // note that msg.value is in wei; need to convert to
-        // ether
-        require( (msg.value / (1 ether)) >= minWager);
+        // CHECK #1
+        // ensure the max number of players has not been reached
+        require(
+            numberOfPlayers < MAX_NUMBER_OF_PLAYERS,
+            "Maximum number of players reached"
+        );
+
+        // CHECK #2
+        // check to ensure caller has never betted
+        require(
+            playerDetailsMapping[msg.sender].numberWagered == 0,
+            "You have already betted"
+        );
+        // CHECK #3
+        // check the range of numbers allowed for betting
+        require(
+            number >= 1 && number <= MAX_NUMBER_TO_BET,
+            string.concat(
+                "You need to bet between 1 and ",
+                Strings.toString(MAX_NUMBER_TO_BET)
+            )
+        );
+
+        // CHECK #4
+        // ensure min. bet (note that msg.value is in wei)
+        require(
+            msg.value >= MIN_WAGER,
+            string.concat(
+                "Minimum bet is ",
+                Strings.toString(MIN_WAGER),
+                " wei"
+            )
+        );
 
         // record the number and amount wagered by the player
-        playerDetails[msg.sender].amountWagered = msg.value;
-        playerDetails[msg.sender].numberWagered = number;
+        playerDetailsMapping[msg.sender].amountWagered = msg.value;
+        playerDetailsMapping[msg.sender].numberWagered = number;
 
-        // add the player address to the array of addresses as
-        // well as mapping
-        playerAddresses.push(payable(msg.sender));
-        playerAddressesMapping[msg.sender] = true;
-
-        numberOfWagers++;
+        // add the player address to the array of addresses
+        playerAddressesArray[numberOfPlayers] = payable(msg.sender);
+        numberOfPlayers++;
         totalWager += msg.value;
 
-        if (numberOfWagers >= MAX_NUMBER_OF_WAGERS) {
-            _announceWinners(); 
-            
-            //---start a new game---
-            // remove all the player addresses mappings
-            _removeAllPlayersFromMapping();
-            
-            // remove all the addresses in the array
-            delete playerAddresses;
-            
-            // reset the variables
-            totalWager = 0;                     
-            numberOfWagers = 0;
-            winningNumber = 999;  
-        }
-        
-        // call the event to inform the client about the 
-        // status of the game
-        emit Status(numberOfWagers, MAX_NUMBER_OF_WAGERS);
+        // emit the Status event
+        emit Status(numberOfPlayers, MAX_NUMBER_OF_PLAYERS);
     }
 
-    function _removeAllPlayersFromMapping() private {
-        for (uint i=0; i < playerAddresses.length; i++) {
-            delete playerAddressesMapping[playerAddresses[i]];
-        }
-    }
+    function announceWinners(uint winningNumber) public {
+        require(msg.sender == owner, "Only the owner can announce the winner");
 
-    function _announceWinners() private {
-        winningNumber = 
-          uint(keccak256(abi.encodePacked(block.timestamp))) % 
-          MAX_WINNING_NUMBER + 1;
-        
-        // call the event to announce the winning number
-        emit WinningNumber(winningNumber);
+        winningBetNumber = winningNumber;
 
-        address payable[MAX_NUMBER_OF_WAGERS] memory winners;
-
+        // use to store winners
+        address payable[MAX_NUMBER_OF_PLAYERS] memory winners;
         uint winnerCount = 0;
         uint totalWinningWager = 0;
-        
+
         // find out the winners
-        for (uint i=0; i < playerAddresses.length; i++) {
+        for (uint i = 0; i < playerAddressesArray.length; i++) {
             // get the address of each player
-            address payable playerAddress = 
-                playerAddresses[i];
-            
-            // if the player betted number is the winning 
-            // number
-            if (playerDetails[playerAddress].numberWagered == 
-                winningNumber) {
-                // save the player address into the winners 
+            address payable playerAddress = playerAddressesArray[i];
+
+            // if the player betted number is the winning number
+            if (
+                playerDetailsMapping[playerAddress].numberWagered ==
+                winningNumber
+            ) {
+                // save the player address into the winners
                 // array
-                winners[winnerCount] = playerAddress;            
-                
-                // sum up the total wagered amount for the 
+                winners[winnerCount] = playerAddress;
+
+                // sum up the total wagered amount for the
                 // winning numbers
-                totalWinningWager +=                     
-                  playerDetails[playerAddress].amountWagered;
+
+                totalWinningWager += playerDetailsMapping[playerAddress]
+                    .amountWagered;
                 winnerCount++;
             }
-        }    
+        }
 
         // make payments to each winning player
-        for (uint j=0; j<winnerCount; j++) {
-            winners[j].transfer(
-                (playerDetails[winners[j]].amountWagered / 
-                 totalWinningWager) * totalWager);
+        for (uint j = 0; j < winnerCount; j++) {
+            // calculate thw winnings of each player
+            uint amount = (playerDetailsMapping[winners[j]].amountWagered *
+                totalWager) / totalWinningWager;
+
+            // make payment to player
+            winners[j].transfer(amount);
+            emit Winner(winners[j], amount);
         }
-        
-        // if there is no winner, transfer all
-        // the remaining ethers back to owner
-        if (winnerCount==0) {
-            owner.transfer(address(this).balance);
+
+        // reset the variables
+        numberOfPlayers = 0;
+        totalWager = 0;
+
+        // clear all arrays and mappings
+        for (uint i = 0; i < playerAddressesArray.length; i++) {
+            // get the address of each player
+            address payable playerAddress = playerAddressesArray[i];
+            delete playerDetailsMapping[playerAddress];
+            delete playerAddressesArray[i];
         }
+        // emit the Status event
+        emit Status(numberOfPlayers, MAX_NUMBER_OF_PLAYERS);
     }
 
-    function getWinningNumber() view public returns (uint) {
-        return winningNumber;
+    function getGameStatus() public view returns (uint, uint) {
+        // return the status of the game
+        return (numberOfPlayers, MAX_NUMBER_OF_PLAYERS);
     }
 
-    //function kill() public {
-    //    if (msg.sender == owner) {
-    //        selfdestruct(owner);
-    //    }        
-    //}
-    
-    function getStatus() view public returns (uint,uint) {
-        return (numberOfWagers, MAX_NUMBER_OF_WAGERS);
-    }    
+    function getWinningNumber() public view returns (uint) {
+        // return the winning number
+        return winningBetNumber;
+    }
 
+    function cashOut() public {
+        require(
+            msg.sender == owner,
+            "Only the owner of contract can cash out!"
+        );
+        payable(owner).transfer(address(this).balance);
+    }
 }
--- test/Lottery.js
+++ test/Lottery.js
@@ -1,8 +1,29 @@
 const {
-    time,
-    loadFixture,
-  } = require("@nomicfoundation/hardhat-toolbox/network-helpers");
-  const { anyValue } = require("@nomicfoundation/hardhat-chai-matchers/withArgs");
-  const { expect } = require("chai");
+  time,
+  loadFixture,
+} = require("@nomicfoundation/hardhat-toolbox/network-helpers");
+const { anyValue } = require("@nomicfoundation/hardhat-chai-matchers/withArgs");
+const { expect } = require("chai");
 
-  
+describe("Lottery", function () {
+  // We define a fixture to reuse the same setup in every test.
+  // We use loadFixture to run this setup once, snapshot that state,
+  // and reset Hardhat Network to that snapshot in every test.
+  async function deployLotteryFixture() {
+    // Contracts are deployed using the first signer/account by default
+    const [owner, otherAccount] = await ethers.getSigners();
+
+    const Lottery = await ethers.getContractFactory("Lottery");
+    const lottery = await Lottery.deploy();
+
+    return { lottery, owner, otherAccount };
+  }
+
+  describe("Deployment", function () {
+    it("Check the default WinningNumber", async function () {
+      const { lottery } = await loadFixture(deployLotteryFixture);
+
+      expect(await lottery.getWinningNumber()).to.equal(0);
+    });
+  });
+});
